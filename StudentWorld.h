4d0c35b6b0d8534aab14f9cabe4efd9a14fb1c9f
Ashley Zhu <ashleyrawr74@gmail.com>
2020-02-17 12:57:13 -0800

    Added actor pointers & m_level

--- StudentWorld.h
+++ StudentWorld.h
@@ -2,8 +2,10 @@
 #define STUDENTWORLD_H_
 
 #include "GameWorld.h"
+#include "Actor.h"
 #include <string>
-
+#include <list>
+class Actor;
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
 class StudentWorld : public GameWorld
@@ -15,6 +17,10 @@ public:
     virtual void cleanUp();
 
 private:
+    std::list<Actor*> m_actors;
+    Actor* m_socrates;
+
+    int m_level;
 };
 
 #endif // STUDENTWORLD_H_
