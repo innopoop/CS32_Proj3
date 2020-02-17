5e9663438bf90b1bd319f39090cddd4a221e3056
Ashley Zhu <ashleyrawr74@gmail.com>
2020-02-17 12:57:54 -0800

    Right before Gameworld implementation

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -11,12 +11,14 @@ GameWorld* createStudentWorld(string assetPath)
 // Students:  Add code to this file, StudentWorld.h, Actor.h and Actor.cpp
 
 StudentWorld::StudentWorld(string assetPath)
-: GameWorld(assetPath)
-{
+    : GameWorld(assetPath)
+{   
+    m_level = 1;
 }
 
 int StudentWorld::init()
 {
+    m_socrates = new Socrates(0, VIEW_HEIGHT / 2);
     return GWSTATUS_CONTINUE_GAME;
 }
 
@@ -24,8 +26,18 @@ int StudentWorld::move()
 {
     // This code is here merely to allow the game to build, run, and terminate after you hit enter.
     // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+    list<Actor*>::iterator it;
+    it = m_actors.begin();
+    m_socrates->doSomething();
+    while (it != m_actors.end()) {
+        (*it)->doSomething();
+        it++;
+    }
     decLives();
-    return GWSTATUS_PLAYER_DIED;
+    if(!m_socrates->isAlive())
+        return GWSTATUS_PLAYER_DIED;
+
+    return GWSTATUS_CONTINUE_GAME;
 }
 
 void StudentWorld::cleanUp()
