2c690b30617998b981260d9ac100f4fd1b34d2a9
Ashley Zhu <ashleyrawr74@gmail.com>
2020-02-17 12:56:15 -0800

    Update Actor.h
    Right before implementing gameworld

--- Actor.h
+++ Actor.h
@@ -2,63 +2,78 @@
 #define ACTOR_H_
 
 #include "GraphObject.h"
+class GameWorld;
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 class Actor : public GraphObject{
 public:
 	Actor(int imageID, double startX, double startY, int dir = 0 , int depth = 0);
+	virtual ~Actor();
 	virtual void doSomething() = 0;
-	virtual bool isAlive();
+	bool isAlive() const;
+	void isDead();
+	void setWorld(GameWorld* gw);
+	GameWorld* getWorld() const;
 private:
 	bool m_alive;
+	GameWorld* m_gw;
 };
-class HealthActor : public Actor {
+
+class Socrates : public Actor {
 public:
-	HealthActor(int imageID, double startX, double startY, int dir = 0, int depth = 0);
-private:
-	int m_health;
-};
-class Socrates : public HealthActor {
-public:
-	Socrates(int startX, int startY, int dir) :HealthActor(IID_PLAYER, startX, startY, dir){}
+	Socrates(double startX, double startY);
+	virtual ~Socrates();
+	virtual void doSomething();
 private:
 	int m_sprayCharge;
 	int m_flameCharge;
+	int m_health;
 };
 
 //fungus (?)
 //regular salmonella
 //aggressive salmonella
 //e.coli
-class Enemy : public HealthActor {
-public:
-private:
-};
-
-//holds bacterias
-class Pit : public Actor {
-public:
-private:
-};
-
-//disinfectant spray
-//flame
-class Projectile : public Actor {
-public:
-private:
-};
-
-//health goodie
-//flame thrower goodie
-//extra life goodie
-class Goodies : public Actor {
-public:
-private:
-};
+//class Enemy : public Actor {
+//public:
+//	Enemy(int startX, int startY, int dir);
+//private:
+//};
+//
+////holds bacterias
+//class Pit : public Actor {
+//public:
+//private:
+//};
+//
+////disinfectant spray
+////flame
+//class Projectile : public Actor {
+//public:
+//private:
+//};
+//
+////health goodie
+////flame thrower goodie
+////extra life goodie
+//class Goodies : public Actor {
+//public:
+//private:
+//};
 
 //holds food & dirt
 class Misc : public Actor {
 public:
+	Misc(int imageID, double startX, double startY, Direction dir = 0, int depth = 1);
+
+	virtual void doSomething() = 0;
+private:
+};
+
+class Dirt : public Misc {
+public:
+	Dirt(double startX, double startY);
+	virtual void doSomething();
 private:
 };
 #endif // ACTOR_H_
