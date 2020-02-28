390b2367475c16b428195b7a323b7b3b8f4cb056
Ashley Zhu <ashleyrawr74@gmail.com>
2020-02-28 14:10:20 -0800

    before crash

--- StudentWorld.h
+++ StudentWorld.h
@@ -4,25 +4,39 @@
 #include "GameWorld.h"
 #include "Actor.h"
 #include <string>
-#include <list>
-class Actor;
+
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
 
 class StudentWorld : public GameWorld
 {
 public:
     StudentWorld(std::string assetPath);
+    ~StudentWorld();
     virtual int init();
     virtual int move();
     virtual void cleanUp();
 
+    //SELF-DEFINED FUNCTIONS
+    void addActor(Actor* actor);
+    int interact(Actor* actor);
+    void pitCleared();
+    void findFood(Actor* actor, int& x, int& y);
 private:
-    bool findDistance(Actor* actor, int x, int y);
-    std::list<Actor*> m_actors;
-    Actor* m_socrates;
+    int findDistance(double x1, double y1, double x2, double y2);
+    bool inDish(double x1, double y1);
+    bool overlap(double x1, double y1, double x2, double y2);
+    void getValidPoints(double& x1, double& y1, int end = 0);
+    double toRadians(int degrees);
 
-    bool invalid_location;
-    int m_level;
+    std::vector<Actor*> m_actors;
+    Socrates* m_socrates;
+
+    int m_nPits;
+    int m_nDirt;
+    int m_nFood;
+    int m_count;
+    int m_chanceFungus;
+    int m_chanceGoodie;
 };
 
 #endif // STUDENTWORLD_H_
